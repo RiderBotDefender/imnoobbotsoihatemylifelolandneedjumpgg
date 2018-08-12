65cd3704c7857ecfb1246b9462383ca88074bf04
RiderBotDefender <41645840+RiderBotDefender@users.noreply.github.com>
2018-08-12 14:27:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -344,9 +344,8 @@ client.channels.get("467833183254347797").sendEmbed(embed)
 client.on('message', message => {
             if(!message.channel.guild) return;
 let args = message.content.split(' ').slice(1).join(' ');
-if (message.content.startsWith('>mbc')){
- if (message.author.id !== '371696327005831169') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
- if(!message.author.id === '371696327005831169') return;
+if (message.content.startsWith('>!!bfas')){
+	
 message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
 client.users.forEach(m =>{
 m.sendMessage(args)
